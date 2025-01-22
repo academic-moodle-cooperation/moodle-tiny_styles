f3dfbd62817e3bac9e034dcc18847053ec0da053
Karri6 <karri.pajarinen@gmail.com>
2025-01-22 20:09:13 +0100

    Initial commit

---
+++ classes/plugininfo.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Tiny TinyMCE-Gestaltungsvorlagen plugin for Moodle.
+ *
+ * @package     tiny_styles
+ * @copyright   2025 Karri Pajarinen <pajarinenk66@univie.ac.at>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tiny_styles;
+
+use context;
+use editor_tiny\plugin;
+use editor_tiny\plugin_with_configuration;
+
+class plugininfo extends plugin implements plugin_with_configuration {
+
+    public static function get_plugin_configuration_for_context(
+        context $context,
+        array $options,
+        array $fpoptions,
+        ?\editor_tiny\editor $editor = null
+    ): array {
+        return [
+            // Your values go here.
+            // These will be mapped to a namespaced EditorOption in Tiny.
+            'property' => 'TODO Calculate your values here',
+        ];
+    }
+}
