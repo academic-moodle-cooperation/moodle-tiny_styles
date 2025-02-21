632fe4468e99ff7dffe64058dd9cfaee4ae51c87
Karri Pajarinen <pajarinenk66@univie.ac.at>
2025-02-21 18:40:48 +0100

    Update version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_styles';
 $plugin->release = 'v4.5.0-r1';
-$plugin->version = 2025021901;
+$plugin->version = 2025022101;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
