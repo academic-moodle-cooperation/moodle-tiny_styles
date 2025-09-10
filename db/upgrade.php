f4ec5493602d1e90b3175d1bc03cc7415d9da61a
Karri Pajarinen <pajarinenk66@univie.ac.at>
2025-09-10 11:49:01 +0000

    Update upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -34,17 +34,6 @@ function xmldb_tiny_styles_upgrade($oldversion = 0) {
 
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2025022701) {
-        $table = new xmldb_table('tiny_styles_elements');
-        $field = new xmldb_field('custom', XMLDB_TYPE_INT, '1', null, XMLDB_NOTNULL, null, '0', 'enabled');
-
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        upgrade_plugin_savepoint(true, 2025022701, 'tiny', 'styles');
-    }
-
     if ($oldversion < 2025041002) {
 
         $table = new xmldb_table('tiny_styles_categories');
