5d83076dd6ab9a8c1e82c44de5fa903efb08f3b7
Karri <pajarinenk66@univie.ac.at>
2025-10-15 15:25:33 +0200

    Initial branch commit

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_styles';
-$plugin->release = 'v5.0.1';
-$plugin->version = 2025080601.01;
+$plugin->release = 'v5.0.2';
+$plugin->version = 2025080601.04;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
